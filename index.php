cc20f897d6ab71089b87e68467abc9e40101e036
Ana Clara <164914557+claraasx@users.noreply.github.com>
2024-07-19 15:40:13 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+Comando While(ENQUANTO)
+
+ 
+a) 	Crie um programa que solicite ao usuário para inserir um número inteiro positivo e, em seguida, exiba todos os números pares entre 0 e o número inserido.
+
+b)	Desenvolva um programa que solicite ao usuário para inserir um número inteiro e, em seguida, exiba a tabuada desse número até o número 10.
+
+c) 	Crie um programa que solicite ao usuário para inserir uma sequência de números inteiros e, em seguida, exiba a soma desses números.
+
+d)	Desenvolva um programa que solicite ao usuário para inserir uma senha e, em seguida, permita que o usuário tente inserir a senha até três vezes. Caso o usuário acerte a senha, exiba uma mensagem de sucesso. Caso contrário, exiba uma mensagem informando que a senha está incorreta.
+ 
+e) 	Crie um programa que solicite ao usuário para inserir números inteiros até que um número negativo seja inserido. Em seguida, exiba a soma dos números inseridos (excluindo o número negativo).
+
+
+Do While(Faça Enquanto)
+
+
+a)	Crie um programa que solicite ao usuário para inserir um número inteiro positivo e, em seguida, exiba todos os números pares entre 0 e o número inserido.
+
+b)	Desenvolva um programa que solicite ao usuário para inserir um número inteiro e, em seguida, exiba a tabuada desse número até o número 10.
+
+c)	Crie um programa que solicite ao usuário para inserir uma sequência de números inteiros e, em seguida, exiba a soma desses números.
+
+d)	Desenvolva um programa que solicite ao usuário para inserir uma senha e, em seguida, permita que o usuário tente inserir a senha até três vezes. Caso o usuário acerte a senha, exiba uma mensagem de sucesso. Caso contrário, exiba uma mensagem informando que a senha está incorreta.
+
+e)	Crie um programa que solicite ao usuário para inserir números inteiros até que um número negativo seja inserido. Em seguida, exiba a soma dos números inseridos (excluindo o número negativo).
+
+f)	Escreva um programa que gere números aleatórios entre 1 e 100 até que o número 50 seja gerado. O programa deve contar quantos números foram gerados até que o 50 aparecesse.
+
+g)	Escreva um programa que leia uma senha do usuário e continue solicitando até que a senha correta seja digitada. A senha correta é "1234".
+
+h)	Escreva um programa que leia uma série de números fornecidos pelo usuário e exiba a média. O programa deve parar de ler quando o usuário digitar um número negativo.
